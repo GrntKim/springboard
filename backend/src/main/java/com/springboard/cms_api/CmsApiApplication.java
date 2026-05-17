cece087fa38a03330354b0f73aceac233eeaa1d1
GrntKim <grantjun0224@gmail.com>
2026-05-17 13:59:01 +0900

    Set up Spring Boot, React, and MySQL Docker environment

---
+++ backend/src/main/java/com/springboard/cms_api/CmsApiApplication.java
@@ -0,0 +1,14 @@
+package com.springboard.cms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Spring!");
+		SpringApplication.run(CmsApiApplication.class, args);
+	}
+
+}
