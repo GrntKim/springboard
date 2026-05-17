23a73c5d3bdb8a3cfcedd21214d464ec329fa4fa
GrntKim <grantjun0224@gmail.com>
2026-05-18 00:26:54 +0900

    Add some more post creating API tests when given blank or null parameters

--- backend/src/test/java/com/springboard/cms_api/post/PostControllerTest.java
+++ backend/src/test/java/com/springboard/cms_api/post/PostControllerTest.java
@@ -92,4 +92,60 @@ class PostControllerTest extends ControllerTestSupport {
         // then
         result.andExpect(status().isCreated());
     }
+
+    @Test
+    void createPost_withBlankTitle_returnsBadRequest() throws Exception {
+        //given
+        String url = "/api/posts";
+        CreatePostRequest request = new CreatePostRequest(
+                testUserId,
+                "",
+                "New Content"
+        );
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createPost_withBlankContent_returnsBadRequest() throws Exception {
+        // given
+        String url = "/api/posts";
+        CreatePostRequest request = new CreatePostRequest(
+                testUserId, "", "New content"
+        );
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createPost_withNullUserId_returnsBadRequest() throws Exception {
+        // given
+        String url = "/api/posts";
+        CreatePostRequest request = new CreatePostRequest(
+                null, "New title", "New content"
+        );
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isBadRequest());
+    }
 }
