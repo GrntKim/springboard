d8afa92b45faa45c470f1984d53b44dde9b2fa72
GrntKim <grantjun0224@gmail.com>
2026-05-18 20:04:48 +0900

    Add minimal style

--- frontend/src/layouts/Header/Header.tsx
+++ frontend/src/layouts/Header/Header.tsx
@@ -1,3 +1,4 @@
+import "./Header.css";
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
@@ -9,6 +10,7 @@ export default function Header() {
 
             <div className="site-menu">
                 <ul className="site-links">
+                    <li><NavLink to={"/"}>Main</NavLink></li>
                     <li><NavLink to={"/posts"}>Posts</NavLink></li>
                     <li><NavLink to={"/write"}>Write</NavLink></li>
                 </ul>
